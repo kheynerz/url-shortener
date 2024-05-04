2052b6888b7222f7b3aecd69e5e36d83e65124f1
kheynerz <sebastiancr44@gmail.com>
2024-05-03 21:15:27 -0600

    Configure import alias and create "Header" component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Header from 'src/components/Header'
 
 const App = () => {
   return (
-    <></>
+    <>
+      <Header/>
+    </>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>URL Shortener</h1>
+      <p>Shorten your URLs with this simple tool.</p>
+    </header>
+  )
+}
+  
+export default Header
